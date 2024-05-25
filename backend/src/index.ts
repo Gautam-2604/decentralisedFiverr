2dd8e2811fbe47d080cabbe4b54f7ff4f285b452
Gautam-2604 <gaurisariag@gmail.com>
2024-05-25 10:24:06 +0530

    complete backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import  userRouter from './routers/user'
 import workerRouter from './routers/worker'
 export const JWT_SECRET = "gg12345"
+import cors from "cors";
 const app = express()
 
 //postgres+ prisma => ORM
 
 app.use("/v1/user", userRouter)
 app.use("/v1/workers",workerRouter)
+app.use(cors())
 
 app.listen(8080)
 
--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,23 +1,49 @@
-import jwt, { decode } from 'jsonwebtoken'
 import { NextFunction, Request, Response } from "express";
-import { JWT_SECRET } from '.';
-export function authMiddleware(req: Request,res: Response,next: NextFunction){
-    const authHeader = req.headers['authorization']??"";
-   try {
-    const decoded = jwt.verify(authHeader, JWT_SECRET)
-    //@ts-ignore
-    if (decoded.userId){
-        //@ts-ignore
-        req.userId = decoded.userId;
-        return next()
-    }else{
+import { JWT_SECRET, WORKER_JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers["authorization"] ?? "";
+
+    try {
+        const decoded = jwt.verify(authHeader, JWT_SECRET);
+        console.log(decoded);
+        // @ts-ignore
+        if (decoded.userId) {
+            // @ts-ignore
+            req.userId = decoded.userId;
+            return next();
+        } else {
+            return res.status(403).json({
+                message: "You are not logged in"
+            })    
+        }
+    } catch(e) {
         return res.status(403).json({
-            message:"You are not logged in"
+            message: "You are not logged in"
         })
     }
-   } catch (error) {
-    return res.status(403).json({message:error})
-   }
 }
 
-export default authMiddleware
+export function workerMiddleware(req: Request, res: Response, next: NextFunction) { 
+    const authHeader = req.headers["authorization"] ?? "";
+
+    console.log(authHeader);
+    try {
+        const decoded = jwt.verify(authHeader, WORKER_JWT_SECRET);
+        // @ts-ignore
+        if (decoded.userId) {
+            // @ts-ignore
+            req.userId = decoded.userId;
+            return next();
+        } else {
+            return res.status(403).json({
+                message: "You are not logged in"
+            })    
+        }
+    } catch(e) {
+        return res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
+}
